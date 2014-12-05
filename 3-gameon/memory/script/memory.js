eccd80da538ce12ff3072f2e7361e2d395943296
jc222fi <jc222fi@student.lnu.se>
2014-12-05 05:06:58 +0000

    table generated, no pics

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -5,8 +5,34 @@ var MemoryGame = {
     
     init: function(){
         
-        MemoryGame.memoryBoard = new RandomGenerator.getPictureArray(4, 2); //Genererar 4 rader i 2 kolumner
-        console.log(MemoryGame.memoryBoard);
+        //MemoryGame.memoryBoard = new RandomGenerator.getPictureArray(4, 2); //Genererar 4 rader i 2 kolumner
+        
+        MemoryGame.createGameBoard();
+        
+    },
+    
+    createGameBoard: function(){
+        
+        var gameBoard = document.getElementById("memorygame");
+        var rows = 4;
+        var cols = 2;
+        var rowCount = "";
+        var cellCount = "";
+        var tableRow = "";
+        var tableCell = "";
+        MemoryGame.memoryBoard = new RandomGenerator.getPictureArray(rows, cols); //Genererar 4 rader i 2 kolumner
+        
+        for(rowCount=0; rowCount<rows; rowCount++){
+            tableRow = document.createElement("TR");
+            gameBoard.appendChild(tableRow);
+            for(cellCount=0; cellCount<cols; cellCount++){
+                tableCell = document.createElement("TD");
+                tableRow.appendChild(tableCell);
+                tableCell.innerHTML = MemoryGame.memoryBoard(rowCount, cellCount);
+            }
+        }
+        
+        console.log(rowCount);
     },
 };
 
