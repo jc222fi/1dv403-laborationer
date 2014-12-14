3783eb7137943380d8c28c3cb8a27cdfcf897665
jc222fi <jc222fi@student.lnu.se>
2014-12-14 22:19:57 +0000

    fixed last of vandalen

--- 4-thequiz/script.js
+++ 4-thequiz/script.js
@@ -1,28 +1,70 @@
 "use strict";
 
-window.onload = function(){
+var MyForm = {
     
-    var xhr = new XMLHttpRequest();
-    var questionArray = [];
-        
-    xhr.onreadystatechange = function(){
+    init: function(){
+        MyForm.getData();
+        MyForm.sendData();
+    },
+    
+    message: function(){
       
-        if(xhr.readyState === 4 && xhr.status === 200){
+      //Här ska det hända lite grejer
+      
+        
+    },
+    
+    getData: function(){
+        var xhr = new XMLHttpRequest();
+        var currentQuestion = "";
             
-            var data = JSON.parse(xhr.responseText);
-            questionArray.push(data.question);
-            console.log(data);
-            var pTag = document.createElement("P");
-            pTag.innerHTML = data.question;
-            var div = document.getElementById("message");
-            div.appendChild(pTag);
-        
+        xhr.onreadystatechange = function(){
+          
+            if(xhr.readyState === 4){
+                if(xhr.status === 200||xhr.status === 304){
+                    var data = JSON.parse(xhr.responseText);
+                    currentQuestion = data.question;
+                    console.log(data);
+                    var pTag = document.createElement("P");
+                    pTag.innerHTML = currentQuestion;
+                    var div = document.getElementById("message");
+                    div.appendChild(pTag);
+                }
+                else{
+                    console.log("FEL");
+                }
+            }
         };
-      
-    };
+        
+        xhr.open("GET", "http://vhost3.lnu.se:20080/question/1", true);
+        xhr.send(null);
+    },
     
-    xhr.open("GET", "http://vhost3.lnu.se:20080/question/1", true);
-    xhr.send(null);
-    
-    
+    sendData: function(){
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function(){
+          
+            if(xhr.readyState === 4){
+                if(xhr.status === 200||xhr.status === 304){
+                
+                    var data = JSON.parse(xhr.responseText);
+                    console.log(data);
+                    var pTag = document.createElement("P");
+                    pTag.innerHTML = "";
+                    var div = document.getElementById("message");
+                    div.appendChild(pTag);
+                }
+                else{
+                    console.log("FEL");
+                }
+            }
+        };
+        xhr.open("POST", "http://vhost3.lnu.se:20080/answer/1", true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        var answer = {
+            answer:2
+        }; 
+        xhr.send(JSON.stringify(answer));
+    }
 };
+window.onload = MyForm.init;
