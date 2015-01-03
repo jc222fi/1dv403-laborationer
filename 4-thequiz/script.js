fb1a9f91255f2dc3b391840902aed3c4260f8f6b
jc222fi <jc222fi@student.lnu.se>
2015-01-03 16:26:31 +0000

    quiz works, time for bugfixing

--- 4-thequiz/script.js
+++ 4-thequiz/script.js
@@ -2,16 +2,11 @@
 
 var MyForm = {
     
+    nextURL: "http://vhost3.lnu.se:20080/question/1",
     init: function(){
+        
         MyForm.createForm();
         MyForm.getData();
-        MyForm.sendData();
-    },
-    
-    message: function(){
-      
-      //Här ska det hända lite grejer
-      
         
     },
     
@@ -20,7 +15,7 @@ var MyForm = {
         var input = document.createElement("input");
         input.setAttribute("id", "answer");
         var button = document.createElement("button");
-        button.setAttribute("id", "submit")
+        button.setAttribute("id", "submit");
         button.innerHTML = "Skicka";
         var pQuestion = document.createElement("p");
         pQuestion.setAttribute("id", "question");
@@ -34,15 +29,17 @@ var MyForm = {
     getData: function(){
         var xhr = new XMLHttpRequest();
         var pQuestion = document.getElementById("question");
-        
+        var data = "";
             
         xhr.onreadystatechange = function(){
           
             if(xhr.readyState === 4){
                 if(xhr.status === 200||xhr.status === 304){
-                    var data = JSON.parse(xhr.responseText);
+                    data = JSON.parse(xhr.responseText);
                     pQuestion.innerHTML = data.question;
                     console.log(data);
+                    MyForm.nextURL = data.nextURL;
+                    MyForm.sendData();
                     
                 }
                 else{
@@ -51,14 +48,17 @@ var MyForm = {
             }
         };
         
-        xhr.open("GET", "http://vhost3.lnu.se:20080/question/1", true);
+        xhr.open("GET", MyForm.nextURL, true);
         xhr.send(null);
+        //MyForm.nextURL = data.nextURL;
+        //console.log(MyForm.nextURL);
     },
     
     sendData: function(){
         var xhr = new XMLHttpRequest();
         var answer = document.getElementById("answer");
         var submit = document.getElementById("submit");
+        var data = "";
         
         submit.onclick = function(){
             xhr.onreadystatechange = function(){
@@ -66,25 +66,24 @@ var MyForm = {
                 if(xhr.readyState === 4){
                     if(xhr.status === 200||xhr.status === 304){
                     
-                        var data = JSON.parse(xhr.responseText);
+                        data = JSON.parse(xhr.responseText);
                         console.log(data);
-                        var pTag = document.createElement("P");
-                        pTag.innerHTML = "";
-                        var div = document.createElement("DIV");
-                        div.appendChild(pTag);
+                        MyForm.nextURL = data.nextURL;
+                        MyForm.getData();
+                        
                     }
                     else{
                         console.log("FEL POST");
                     }
                 }
             };
-            xhr.open("POST", "http://vhost3.lnu.se:20080/answer/1", true);
+            xhr.open("POST", MyForm.nextURL, true);
             xhr.setRequestHeader("Content-Type", "application/json");
             var returnData = {
                 "answer": answer.value
             }; 
             xhr.send(JSON.stringify(returnData));
-        }
+        };
     }
 };
 window.onload = MyForm.init;
