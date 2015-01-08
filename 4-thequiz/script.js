3560092548b8b74d112e68fb9c1996424576bcf5
jc222fi <jc222fi@student.lnu.se>
2015-01-08 16:18:18 +0000

    wrong answer working

--- 4-thequiz/script.js
+++ 4-thequiz/script.js
@@ -4,6 +4,7 @@ var MyForm = {
     
     nextURL: "http://vhost3.lnu.se:20080/question/1",
     counter: 0,
+    question: "",
     init: function(){
         
         MyForm.createForm();
@@ -37,7 +38,8 @@ var MyForm = {
             if(xhr.readyState === 4){
                 if(xhr.status === 200||xhr.status === 304){
                     data = JSON.parse(xhr.responseText);
-                    pQuestion.innerHTML = data.question;
+                    MyForm.question = data.question;
+                    pQuestion.innerHTML = MyForm.question;
                     console.log(data);
                     MyForm.nextURL = data.nextURL;
                     MyForm.sendData();
@@ -63,9 +65,11 @@ var MyForm = {
             xhr.onreadystatechange = function(){
               
                 if(xhr.readyState === 4){
+                    MyForm.counter +=1;
+                    console.log(MyForm.counter);
                     data = JSON.parse(xhr.responseText);
                     if(xhr.status === 200||xhr.status === 304){
-                    
+                        
                         console.log(data.message);
                         MyForm.nextURL = data.nextURL;
                         MyForm.getData();
@@ -74,7 +78,7 @@ var MyForm = {
                         var question = document.getElementById("question");
                         question.innerHTML = data.message;
                         setTimeout(function(){
-                            question.innerHTML = data.question;
+                            question.innerHTML = MyForm.question;
                         }, 1000);
                     }
                 }
