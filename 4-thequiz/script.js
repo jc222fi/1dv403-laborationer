31f0c22f5415a0e5eb99ad25b65ccf2897481a5c
jc222fi <jc222fi@student.lnu.se>
2015-01-08 18:01:01 +0000

    fixed some details

--- 4-thequiz/script.js
+++ 4-thequiz/script.js
@@ -12,40 +12,46 @@ var MyForm = {
         
     },
     
+    //skapar formuläret för applikationen
     createForm: function(){
         var div = document.getElementById("container");
         var input = document.createElement("input");
         input.setAttribute("id", "answer");
+        
         var button = document.createElement("button");
         button.setAttribute("id", "submit");
         button.innerHTML = "Skicka";
-        var pQuestion = document.createElement("p");
-        pQuestion.setAttribute("id", "question");
         
-        div.appendChild(pQuestion);
+        var pText = document.createElement("p");
+        pText.setAttribute("id", "text");
+        
+        div.appendChild(pText);
         div.appendChild(input);
         div.appendChild(button);
         
     },
     
+    //funktionalitet för att ta emot data
     getData: function(){
         var xhr = new XMLHttpRequest();
-        var pQuestion = document.getElementById("question");
+        var pQuestion = document.getElementById("text");
         var data = "";
             
         xhr.onreadystatechange = function(){
-          
+            
             if(xhr.readyState === 4){
+                //om allt är ok och vi kommunicerar med servern hanterar vi datan vi får
                 if(xhr.status === 200||xhr.status === 304){
                     data = JSON.parse(xhr.responseText);
                     MyForm.question = data.question;
                     pQuestion.innerHTML = MyForm.question;
-                    console.log(data);
                     MyForm.nextURL = data.nextURL;
                     MyForm.sendData();
                     
                 }
+                //annars kollar vi om det är slut på frågor eller om nåt gått fel
                 else{
+                    //om frågorna är slut är quizet slut, statistik om antal gissningar per fråga presenteras
                     if(MyForm.nextURL === undefined){
                         console.log("Spelet slut");
                         for(var i=0; i<MyForm.counterArray.length;i++){
@@ -54,6 +60,7 @@ var MyForm = {
                             pResult.innerHTML = "På fråga "+(i+1)+" krävdes "+MyForm.counterArray[i]+" försök.";
                         }
                     }
+                    //annars är något fel
                     else{
                         console.log("FEL GET");
                     }
@@ -65,13 +72,16 @@ var MyForm = {
         xhr.send(null);
     },
     
+    //funktionalitet för att skicka data
     sendData: function(){
         var xhr = new XMLHttpRequest();
         var answer = document.getElementById("answer");
         var submit = document.getElementById("submit");
+        var text = document.getElementById("text");
         var data = "";
         var counter = 0;
         
+        //skicka data först när användaren klickar på knappen
         submit.onclick = function(){
             xhr.onreadystatechange = function(){
               
@@ -79,18 +89,20 @@ var MyForm = {
                     counter +=1;
                     console.log(counter);
                     data = JSON.parse(xhr.responseText);
+                    //om användaren svarar rätt vill vi ha fram nästa fråga
                     if(xhr.status === 200||xhr.status === 304){
                         MyForm.counterArray.push(counter);
-                        console.log(data.message);
-                        console.log(MyForm.counterArray);
+                        text.innerHTML = data.message;
                         MyForm.nextURL = data.nextURL;
-                        MyForm.getData();
-                    }
-                    else{
-                        var question = document.getElementById("question");
-                        question.innerHTML = data.message;
                         setTimeout(function(){
-                            question.innerHTML = MyForm.question;
+                            MyForm.getData();
+                        }, 1000);
+                    }
+                    //annars är det fel och vi vill ha samma fråga igen
+                    else{
+                        text.innerHTML = data.message;
+                        setTimeout(function(){
+                            text.innerHTML = MyForm.question;
                         }, 1000);
                     }
                 }
