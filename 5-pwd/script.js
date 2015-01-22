f0855b4e6ff631e7bce6aa204ff66e6879a6e70f
jc222fi <jc222fi@student.lnu.se>
2015-01-22 03:30:52 +0000

    window can be closed and opened

--- 5-pwd/script.js
+++ 5-pwd/script.js
@@ -5,12 +5,19 @@ var MyDesktop = {
     
     init: function(){
         
+        MyDesktop.openDesktop();
+    },
+    
+    openDesktop: function(){
+        
         var button = document.createElement("a");
         var buttonImage = document.createElement("img");
-        var footer = document.querySelector("footer");
+        var footer = document.createElement("footer");
+        var container = document.getElementById("container");
         
         button.href = "#";
         buttonImage.src = "pics/desktopicon.png";
+        container.appendChild(footer);
         footer.appendChild(button);
         button.appendChild(buttonImage);
         
@@ -18,7 +25,6 @@ var MyDesktop = {
             
             MyDesktop.openWindow();
         };
-        
     },
     
     openWindow: function(){
@@ -29,25 +35,38 @@ var MyDesktop = {
         var windowBody = document.createElement("div");
         var windowFooter = document.createElement("div");
         var windowHeadline = document.createElement("p");
-        //var statusLine = document.createElement("p");
+        var statusLine = document.createElement("p");
         var headerIcon = document.createElement("img");
+        var closeIcon = document.createElement("img");
+        var a = document.createElement("a");
         
+        a.href ="#";
+        closeIcon.src = "pics/cancel.png";
         headerIcon.src = "pics/desktopicon.png";
+        headerIcon.setAttribute("class", "headerIcon");
+        a.setAttribute("class", "close");
         windowDiv.setAttribute("class", "imageViewer");
         windowBody.setAttribute("class", "windowBody");
         windowHeader.setAttribute("class", "windowHeader");
         windowFooter.setAttribute("class", "windowFooter");
         
+        a.appendChild(closeIcon);
         windowHeader.appendChild(headerIcon);
+        windowHeader.appendChild(a);
         windowHeader.appendChild(windowHeadline);
-        //windowFooter.appendChild(statusLine);
+        windowFooter.appendChild(statusLine);
         windowDiv.appendChild(windowHeader);
         windowDiv.appendChild(windowBody);
         windowDiv.appendChild(windowFooter);
         containerDiv.appendChild(windowDiv);
         
         windowHeadline.innerHTML = "Image Viewer";
-        //console.log(divContainer);
+        statusLine.innerHTML = "Status..."
+        
+        a.onclick = function(){
+            containerDiv.innerHTML = "";
+            MyDesktop.openDesktop();
+        }
     }
     
 };
